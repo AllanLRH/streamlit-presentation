869af6d72d8c86f54503afdf36349a0e8ac627f8
Allan Leander Rostock Hansen <allh@nuuday.dk>
2021-01-20 16:34:48 +0100

    Update cache code

--- snippets/05-cached-results.py
+++ snippets/05-cached-results.py
@@ -1,8 +1,10 @@
-import streamlit as st
-import pandas as pd
 import time
-import seaborn as sns
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import streamlit as st
 
 
 @st.cache
@@ -23,7 +25,19 @@ if st.checkbox("Show raw data", False):
         "How should the data be shown?", ["Interactive DataFrame", "Static table"]
     )
     if show_as == "Interactive DataFrame":
-        st.dataframe(df)
+        if st.button("Toggle heatmap on `shape__length` column"):
+            format_dict = {"shape__length": "{0:,.1f}"}
+            msg = """Pandas' data can be styled when rendering it in a webbrowser; this is a Pandas feature that
+                     you can read a blog post about over at [Practical Business Python](https://pbpython.com/styling-pandas.html).
+                  """
+            st.markdown(msg)
+            st.dataframe(
+                df.style.format(format_dict).background_gradient(
+                    subset=["shape__length"], cmap="BuGn"
+                )
+            )
+        else:
+            st.dataframe(df)
     elif show_as == "Static table":
         n_rows = st.slider("How many rows?", 5, 100, 8)
         st.table(df.head(n_rows))
@@ -33,9 +47,10 @@ st.subheader("Check the boxes to see histograms")
 n_std = st.slider(
     "How many standard deviations from the median should be allowed?", 1, 10, 10
 )
+grid_bool = st.checkbox("Toggle grid")
 med, std = df.shape__length.median(), df.shape__length.std()
 dfc = df.query(
-    f"(shape__length > (@med - {n_std}*@std)) & (shape__length < (@med + {n_std}*@std))"
+    "(shape__length > (@med - @n_std*@std)) & (shape__length < (@med + @n_std*@std))"
 )
 
 fig, ax = plt.subplots(figsize=[8, 4])
@@ -46,4 +61,27 @@ sns.violinplot(
     linewidth=0,
     ax=ax,
 )
+if grid_bool:
+    ax.grid()
+
 st.pyplot(ax.get_figure())
+
+if st.button("Toggle details about caching"):
+    msg = """
+    ### The following elements are tracked for cache invalidation
+
+    1. The input parameters that you called the function with
+    1. The value of any external variable used in the function
+    1. The body of the function
+    1. The body of any function used inside the cached function (but not external libraries)
+
+    ### Good to know about caching
+
+    * A hashmap (dict) is used for caching
+    * Caches are stored by reference
+    * The cached holds retun-value and a hash of said value
+
+    For more information, read the documentation section<br>
+    _[Improve app performance → Advanced Caching](https://docs.streamlit.io/en/stable/caching.html#advanced-caching)_.
+    """
+    st.markdown(msg, unsafe_allow_html=True)
