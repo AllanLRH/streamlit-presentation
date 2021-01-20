411f80b72f81dacc0e382e235dc8dc7a189c8f68
Allan Leander Rostock Hansen <allh@nuuday.dk>
2021-01-20 17:11:46 +0100

    Clean up the caching examples

--- snippets/05-cached-results.py
+++ snippets/05-cached-results.py
@@ -14,8 +14,10 @@ def get_data(delay=0):
     return df
 
 
-st.title("Boston bicycle routes")
+st.title("Boston bicycle routes (caching)")
 df = get_data(3).sort_values("installdat")
+
+# This is for toggeling the display of the dataframe
 if st.checkbox("Show raw data", False):
     show_as = st.selectbox("How should the data be shown?", ["Interactive DataFrame", "Static table"])
     if show_as == "Interactive DataFrame":
@@ -34,7 +36,8 @@ if st.checkbox("Show raw data", False):
         n_rows = st.slider("How many rows?", 5, 100, 8)
         st.table(df.head(n_rows))
 
-st.subheader("Check the boxes to see histograms")
+# This part handles the plotting
+st.subheader("Distribution of ride lengths by year")
 
 n_std = st.slider("How many standard deviations from the median should be allowed?", 1, 10, 10)
 grid_bool = st.checkbox("Toggle grid")
@@ -43,29 +46,13 @@ dfc = df.query("(shape__length > (@med - @n_std*@std)) & (shape__length < (@med 
 
 fig, ax = plt.subplots(figsize=[8, 4])
 sns.violinplot(
-    x=dfc["installdat"], y=dfc["shape__length"], color="teal", linewidth=0, ax=ax,
+    x=dfc["installdat"],
+    y=dfc["shape__length"],
+    color="teal",
+    linewidth=0,
+    ax=ax,
 )
 if grid_bool:
     ax.grid()
 
 st.pyplot(ax.get_figure())
-
-if st.button("Toggle details about caching"):
-    msg = """
-    ### The following elements are tracked for cache invalidation
-
-    1. The input parameters that you called the function with
-    1. The value of any external variable used in the function
-    1. The body of the function
-    1. The body of any function used inside the cached function (but not external libraries)
-
-    ### Good to know about caching
-
-    * A hashmap (dict) is used for caching
-    * Caches are stored by reference
-    * The cached holds retun-value and a hash of said value
-
-    For more information, read the documentation section<br>
-    _[Improve app performance → Advanced Caching](https://docs.streamlit.io/en/stable/caching.html#advanced-caching)_.
-    """
-    st.markdown(msg, unsafe_allow_html=True)
--- snippets/06-cached-result-improved.py
+++ snippets/06-cached-result-improved.py
@@ -7,6 +7,7 @@ import streamlit as st
 
 
 # NOTE: You may need to clear the cache, see the menu top right, or press 'C' in the browser
+# This turns off hashing for this DataFrame, because I KNOW that it's not changing
 @st.cache(hash_funcs={pd.DataFrame: lambda _: None})
 def get_data(delay=0):
     df = pd.read_csv("assets/Existing_Bike_Network.csv").rename(columns=str.lower).set_index("fid")
@@ -14,10 +15,11 @@ def get_data(delay=0):
     return df
 
 
-st.title("Boston bicycle routes")
-df = get_data(3).sort_values("installdat")
-if st.checkbox("Show raw data", False):
+# This is a big chunk of code, so it's a good candidate for a function
+def show_raw_data():
     show_as = st.selectbox("How should the data be shown?", ["Interactive DataFrame", "Static table"])
+    interesting_cols = ["street_nam", "jurisdicti", "installdat", "exisfacil", "shape__length"]
+    df_show = df.loc[:, interesting_cols]  # We make a copy of df as to not mutate it
     if show_as == "Interactive DataFrame":
         if st.button("Toggle heatmap on `shape__length` column"):
             format_dict = {"shape__length": "{0:,.1f}"}
@@ -26,31 +28,53 @@ if st.checkbox("Show raw data", False):
                   """
             st.markdown(msg)
             st.dataframe(
-                df.style.format(format_dict).background_gradient(subset=["shape__length"], cmap="BuGn")
+                df_show.style.format(format_dict).background_gradient(
+                    subset=["shape__length"], cmap="BuGn"
+                )
             )
         else:
-            st.dataframe(df)
+            st.dataframe(df_show)
     elif show_as == "Static table":
         n_rows = st.slider("How many rows?", 5, 100, 8)
-        st.table(df.head(n_rows))
+        st.table(df_show.head(n_rows))
 
-st.subheader("Check the boxes to see histograms")
 
-n_std = st.slider("How many standard deviations from the median should be allowed?", 1, 10, 10)
-grid_bool = st.checkbox("Toggle grid")
+st.title("Boston bicycle routes (improved caching)")
+df = get_data(delay=3).sort_values("installdat")
+
+# This part handles the plotting
+st.subheader("Distribution of ride lengths by year")
+
+# These values are needed later
 med, std = df.shape__length.median(), df.shape__length.std()
+
+# Let's create two collumns
+left_column, right_column = st.beta_columns(2)
+
+n_std = right_column.slider("How many standard deviations from the median should be allowed?", 1, 10, 10)
+show_df: bool = left_column.checkbox("Show raw data", False)
+grid_bool: bool = left_column.checkbox("Toggle plot grid", True)
+
 dfc = df.query("(shape__length > (@med - @n_std*@std)) & (shape__length < (@med + @n_std*@std))")
 
 fig, ax = plt.subplots(figsize=[8, 4])
 sns.violinplot(
-    x=dfc["installdat"], y=dfc["shape__length"], color="teal", linewidth=0, ax=ax,
+    x=dfc["installdat"],
+    y=dfc["shape__length"],
+    color="teal",
+    linewidth=0,
+    ax=ax,
 )
 if grid_bool:
     ax.grid()
 
+
 st.pyplot(ax.get_figure())
 
-if st.button("Toggle details about caching"):
+if show_df:
+    show_raw_data()
+
+if st.sidebar.button("Toggle details about caching"):
     msg = """
     ### The following elements are tracked for cache invalidation
 
@@ -68,4 +92,4 @@ if st.button("Toggle details about caching"):
     For more information, read the documentation section<br>
     _[Improve app performance → Advanced Caching](https://docs.streamlit.io/en/stable/caching.html#advanced-caching)_.
     """
-    st.markdown(msg, unsafe_allow_html=True)
+    st.sidebar.markdown(msg, unsafe_allow_html=True)
