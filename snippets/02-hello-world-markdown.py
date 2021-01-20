0390e258aa6d441596082e7fddc9f523f211eee8
Allan Leander Rostock Hansen <allh@nuuday.dk>
2021-01-20 18:02:01 +0100

    Add image to markdown, show write and markdown

--- snippets/02-hello-world-markdown.py
+++ snippets/02-hello-world-markdown.py
@@ -3,10 +3,29 @@ import streamlit as st
 st.title("Greetings")
 
 msg = """
-# Greetings
+# Greetings (markdown H1)
+## Greetings (markdown H2)
+### Greetings (markdown H3)
+#### Greetings (markdown H4)
+##### Greetings (markdown H5)
 
 Well _hello_ to ya'll!
 
 (Streamlit renders output as **markdown** by default.)
 """
 st.write(msg)
+
+
+msg_with_images = R"""
+## Hello kitties!
+
+<img src="https://www.rspcasa.org.au/wp-content/uploads/2019/07/kittens-hanging-1-1003x1024.jpg" width="250">
+
+<br>
+<br>
+
+![](https://www.rspcasa.org.au/wp-content/uploads/2019/07/kittens-hanging-1-1003x1024.jpg)
+
+"""
+
+st.markdown(msg_with_images, unsafe_allow_html=True)
