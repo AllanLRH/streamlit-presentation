9b7dffc7cd88c286db382f3cd568a8d9c0b4ef6e
Allan Leander Rostock Hansen <allh@nuuday.dk>
2021-01-20 17:19:18 +0100

    Added examples of magic calls

---
+++ snippets/01-hello-magic.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+"""
+# This is the document title
+
+This is some _markdown_.
+"""
+
+df = pd.DataFrame({"col1": [1, 2, 3]})
+df  # <-- Draw the dataframe
+
+x = 10
+"x", x  # <-- Draw the string 'x' and then the value of x
